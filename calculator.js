fc7849effd9477ffd63210fff3ea54a1dd15265b
JC8299 <justincheng.cheng@gmail.com>
2022-08-18 20:14:33 -0700

    Create skeleton for calculator

---
+++ calculator.js
@@ -0,0 +1,19 @@
+function add(left, right) {
+
+}
+
+function subtract(left, right) {
+
+}
+
+function multiply(left, right) {
+
+}
+
+function divide(left, right) {
+
+}
+
+function operate(operator, left, right) {
+    
+}
